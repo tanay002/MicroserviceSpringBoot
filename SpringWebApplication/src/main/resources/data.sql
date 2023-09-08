728d40c132bdf6cc7dcdc3c0bdf8276ca418cd1b
Tanay Saxena <tanay.saxena002@gmail.com>
2023-09-08 16:58:39 +0530

    removind mysql file

--- SpringWebApplication/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-delete from task_todo;
-
-insert into task_todo (username,description,target_date,done) values ('tanay','Learning Spring Framework 6','2023-09-08',0);
-insert into task_todo (username,description,target_date,done) values ('tanay','Learning AWS','2023-11-08',0);
-insert into task_todo (username,description,target_date,done) values ('tanay','Learning Microservices','2023-08-8',1);
-insert into task_todo (username,description,target_date,done) values ('rahul','React JS','2023-10-08',0);
-insert into task_todo (username,description,target_date,done) values ('rahul','Javascript','2023-08-08',1);
-
